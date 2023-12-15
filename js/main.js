fe6c83c13e634cfb13d0798083592d3607cb2570
阿敗塗一地 <peggy1008123@yahoo.com.tw>
2023-12-16 01:35:16 +0800

    project page(side navigation, upper places finished)
    still need to finish the mode that fit the project

--- js/main.js
+++ js/main.js
@@ -28,3 +28,49 @@ function tomywork() {
         inline: 'start'
     });
 }
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this,
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+// 获取目标 div 元素
+const hiddenDiv = document.querySelector('.project_rowblock');
+
+// 创建 Intersection Observer 对象
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // 如果目标 div 进入视口，显示它
+      hiddenDiv.classList.add('active');
+      observer.unobserve(hiddenDiv); // 可选：如果只想触发一次，可以停止观察
+    }
+  });
+}, {
+  // 设置观察器的选项
+  // 可以设置 root、rootMargin、threshold 等选项
+});
+
+// 开始观察目标 div
+observer.observe(hiddenDiv);
+
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('main-header');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 40) { // 当滚动位置超过50像素时
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
---
+++ js/project1.js
@@ -0,0 +1,54 @@
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('main-header');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 40) { // 当滚动位置超过50像素时
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".vertical-menu a");
+
+ navLinks.forEach((link, index) => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const yOffset = -80; // 你想要的额外偏移量
+    const target = sections[index];
+    const targetPosition = target.getBoundingClientRect().top + window.pageYOffset;
+    window.scrollTo({ top: targetPosition + yOffset, behavior: "smooth" });
+  });
+});
+
+  window.addEventListener("scroll", function() {
+  const currentScroll = window.scrollY;
+
+  sections.forEach((section, index) => {
+    const top = section.offsetTop - 160; // 微调滚动位置
+    const bottom = top + section.offsetHeight;
+
+    if (currentScroll >= top && currentScroll < bottom) {
+      navLinks.forEach(link => link.classList.remove("active"));
+      navLinks[index].classList.add("active");
+    }
+  });
+});
+});
+
+const verticalMenu = document.getElementById('vertical-menu');
+const triggerOffset = 600; // 滚动到页面某个位置触发的偏移量
+
+window.addEventListener('scroll', function() {
+  // 获取垂直滚动位置
+  const scrollPosition = window.scrollY || window.pageYOffset;
+
+  // 如果滚动超过指定的偏移量，显示垂直导航栏，否则隐藏
+  if (scrollPosition > triggerOffset) {
+    verticalMenu.classList.add('show-menu');
+  } else {
+    verticalMenu.classList.remove('show-menu');
+  }
+});
